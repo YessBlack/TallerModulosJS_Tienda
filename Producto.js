9f1eb54d8b6e190a6bccd24891e6b84ea873a2b5
Angelica Garcia <70681219+YessBlack@users.noreply.github.com>
2026-07-14 11:34:52 -0500

    add Product class

---
+++ Producto.js
@@ -0,0 +1,25 @@
+class Producto {
+  constructor(nombre, precio, categoria, stock) {
+    this.nombre = nombre
+    this.precio = precio
+    this.categoria = categoria
+    this.stock = stock
+  }
+
+  hayStock(cantidad) {
+    return this.stock >= cantidad
+  }
+
+  ficha() {
+    const estadoStock = this.stock > 0
+      ? `${this.stock} unidades disponibles`
+      : 'Sin stock'
+    return `${this.nombre}
+      - Categoría: ${this.categoria}
+      - Precio: ${this.precio}
+      - Stock: ${estadoStock}
+    `
+  }
+}
+
+module.exports = Producto
