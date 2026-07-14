a5a40399202b043c7e1288d5a712b1158639e018
Angelica Garcia <70681219+YessBlack@users.noreply.github.com>
2026-07-14 12:48:51 -0500

    add index entry point with app function and test scenarios

---
+++ index.js
@@ -0,0 +1,35 @@
+const Carrito = require("./Carrito.js")
+const Usuario = require("./Usuario.js")
+const Producto = require("./Producto.js")
+
+const { esEmailValido } = require("./utilidades.js")
+
+const app = (usuario, productos) => {
+  if (!esEmailValido(usuario.email)) {
+    return console.log('Email No es Valido')
+  }
+
+  const carrito = new Carrito(usuario)
+
+  for (const { producto, cantidad } of productos) {
+    console.log(carrito.agregar(producto, cantidad))
+  }
+
+  console.log(carrito.recibo())
+}
+
+// Prueba 1
+const usuario1 = new Usuario("Jose Maria", "josema@gmail.com", true)
+const productos1 = [
+  { producto: new Producto('Camisa', 25000, 'Ropa', 10), cantidad: 5 },
+  { producto: new Producto('Pantalón', 80000, 'Ropa', 20), cantidad: 25 },
+  { producto: new Producto('Zapatos', 120000, 'Calzado', 15), cantidad: 2 }
+]
+
+// Prueba 2
+const usuario2 = new Usuario("Ana Torres", "ana@gmail.com", false)
+const productos2 = [
+  { producto: new Producto('Camisa', 25000, 'Ropa', 10), cantidad: 2 }
+]
+
+app(usuario2, productos2)
